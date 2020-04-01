de1dd7bb3001d764ed57aa20189d3d5e6e94b1dc
mbichoh <nattymbichoh@gmail.com>
2020-04-01 15:11:20 +0300

    Pesa management app- using android, daraja and africas talkin. Plan, divide and save your money. Main Activity

---
+++ app/src/main/java/com/nathanmbichoh/pesa_management_app/MainActivity.java
@@ -0,0 +1,26 @@
+package com.nathanmbichoh.pesa_management_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnNextPage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnNextPage = (Button) findViewById(R.id.btnGetStarted);
+        btnNextPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
